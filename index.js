f424bed52b896636898d6cc3813fa09debe1f423
Kenneth Kebaara <kkebaara@yahoo.com>
2022-01-18 05:39:37 -0700

    commit tree 69f57fafc48898ddd126d8ee73d44603444a7321

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-// Your code here
-
-
 const dodger = document.getElementById("dodger"); 
 
 function moveDodgerLeft() {
@@ -12,25 +9,20 @@ function moveDodgerLeft() {
     }
   } 
 
-  document.addEventListener("keydown", function(e) {
-    if (e.key === "ArrowLeft") {
-      moveDodgerLeft();
-    }
-  }); 
-
-  // arrow right 
-
-  function moveDodgerRight() {
+function moveDodgerRight() {
     const leftNumbers = dodger.style.left.replace("px", "");
     const left = parseInt(leftNumbers, 10);
   
-    if (left < - 1) {
+    if (left > 0) {
       dodger.style.left = `${left + 1}px`;
+    } 
+}
+
+document.addEventListener("keydown", function(event) {
+    if (event.key === "ArrowLeft") {
+      moveDodgerLeft();
     }
-  } 
-  
-document.addEventListener("keydown", function(e) {
-    if (e.key === "ArrowRight") {
-      moveDodgerRight();
+    if (event.key == "ArrowRight") {
+        moveDodgerRight();
     }
   }); 
